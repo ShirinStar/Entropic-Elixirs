e6b8061ffa4010aa9a6cec1fe25cb59d7c10dc23
shirin <shirinanlen@gmail.com>
2019-06-28 21:52:32 -0400

    mapping thro q set is undefine

--- client-side/src/components/QOne.jsx
+++ client-side/src/components/QOne.jsx
@@ -3,53 +3,54 @@ import questions from './questionsList';
 
 function QOne(props) {
 const { handleNext, userAnswers } = props
-const [ answer_breaking, setAnswer_breaking ] = React.useState('');
-const [ answer_building, setAnswer_building ] = React.useState('');
-const [ answer_with_it, setAnswer_with_it ] = React.useState('');
-const [ answer_against_it, setAnswer_against_it ] = React.useState('');
-const [ answer_intuition, setAnswer_intuition ] = React.useState('');
-const [ answer_intention, setAnswer_intention ] = React.useState('');
+const [ answer_breaking, set_answer_breaking ] = React.useState('');
+const [ answer_building, set_answer_building ] = React.useState('');
+const [ answer_with_it, set_answer_with_it ] = React.useState('');
+const [ answer_against_it, set_answer_against_it ] = React.useState('');
+const [ answer_intuition, set_answer_intuition ] = React.useState('');
+const [ answer_intention, set_answer_intention ] = React.useState('');
 
 
  const handleSubmit = async (e) => {
   e.preventDefault()
   const submit = handleNext;
   const userAnswers= {
-    Answer_breaking,
-    Answer_building,
-    Answer_with_it,
-    Answer_against_it,
-    Answer_intuition,
-    Answer_intention
+    answer_breaking,
+    answer_building,
+    answer_with_it,
+    answer_against_it,
+    answer_intuition,
+    answer_intention
   }
    submit(userAnswers);
 }
 
- // useEffect(async () => {
- //   const res = await ('./questions.js')
- //   console.log(res);
- // })
-
 return (
+  <>
   <div className='div-title'>
     <h1 className='general-title'>Quesion no.1</h1>
+  </div>
+  <div>
+    <h2>{questions.questions[0].title}</h2>
+  </div>
+  <div>
+ {
+   questions.questions[0].answers.map(answer => {
+     return (
+       <div key={answer.text}>
+         <form className='form-q' onSubmit={handleSubmit}>
+           <input type="radio" name='answer' value={answer.value}
+            required onChange={e => set_`${answer.catagory}`(e.target.value)}/>
+            {answer.text}
+         </form>
+       </div>
+     )
+   })
+ }
+ <button className='btn' type='submit'>Next</button>
 
-    <h2>{questions.title}</h2>
-
- </div>
-   // <div className='main-q'>
-   //  {questions.map(question => {
-   //    <h2 className='q-title'>{question.title}</h2>//after this an audio src
-   //    <form className='form-q'onSubmit={handleSubmit}>
-   //    <input type="radio" name='question' value={text.value}
-   //      required onChange={e => set`${catagory}`(e.target.value)}/>
-   //    {answers.text}
-   //    </div>
-   //  })}
-   //  <button className='btn' type='submit'>Next</button>
-
-    // </form>
-    // </div>
+</div>
+</>
  );
 };
 export default QOne;
--- client-side/src/components/questionsList.js
+++ client-side/src/components/questionsList.js
@@ -5,27 +5,27 @@ export default {
     "answers": [
       {
         "text": "This is something I never think about.",
-        "catagory": "Answer_breaking",
+        "catagory": "answer_breaking",
         "value": 1
       },
       {
         "text": "I think about doing work on myself sometimes, but what for?",
-        "catagory": "Answer_breaking",
+        "catagory": "answer_breaking",
         "value": 1
       },
       {
         "text": "I want to do work on myself, but I don’t know how.",
-        "catagory": "Answer_breaking",
+        "catagory": "answer_breaking",
         "value": 1
       },
       {
         "text": "I do work on myself often, but wouldn’t say it’s a central focus of my life.",
-        "catagory": "Answer_building",
+        "catagory": "answer_building",
         "value": 1
       },
       {
         "text": "Working on myself is something I spend considerable time and resources on (either by myself or with the support of others).",
-        "catagory": "Answer_building",
+        "catagory": "answer_building",
         "value": 1
     }
    ]
