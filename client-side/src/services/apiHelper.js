853ee199e3a616d2c0c585bf72548347166e804e
Nolan Hill <nolanehill@gmail.com>
2019-08-19 19:41:00 -0400

    Fix issue where API endpoint needs to be manually updated each time the pi's ip address changes

--- client-side/src/services/apiHelper.js
+++ client-side/src/services/apiHelper.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://192.168.7.49:3001';
+const ip = location.host;
+const BASE_URL = "http://" + ip;
 // const BASE_WS = 'https://dummy-rest-api.glitch.me/user'
 // https://glitch.com/edit/#!/dummy-rest-api?path=server.js:1:0
 
