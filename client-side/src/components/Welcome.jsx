6653ceec17e0656a21dab8706de3e0386bbc0298
shirin <shirinanlen@gmail.com>
2019-09-03 11:36:09 -0400

    fix welcome page

--- client-side/src/components/Welcome.jsx
+++ client-side/src/components/Welcome.jsx
@@ -8,9 +8,7 @@ function Welcome(props) {
     <div className='text-container'>
       <h3 className='welcomeText'>Before you sit, please take a cup from the stack
       and place it on the pedestal on top of the machine</h3>
-      <button className='welcome-btn' onClick={()=>props.history.push('/intro1')}>I did it!</button>
-
-    <img className='cup' src='../img/hand_cupBlue.png' />
+      <button className='welcome-btn' onClick={()=>props.history.push('/intro1')}>Next</button>
     </div>
   )
 }
